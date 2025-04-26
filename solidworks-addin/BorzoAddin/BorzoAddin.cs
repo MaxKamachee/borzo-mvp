c01b9448517f1dfc5724615a33c4ecbec468a840
MaxKamachee <max.kamachee@gmail.com>
2025-04-25 20:34:03 -0500

    Try 3 with integrating into solidworks

--- solidworks-addin/BorzoAddin/BorzoAddin.cs
+++ solidworks-addin/BorzoAddin/BorzoAddin.cs
@@ -3,6 +3,7 @@ using System.Runtime.InteropServices;
 using SolidWorks.Interop.sldworks;
 using SolidWorks.Interop.swconst;
 using System.Windows.Forms;
+using Microsoft.Win32;
 
 namespace BorzoAddin
 {
@@ -51,6 +52,27 @@ namespace BorzoAddin
             var bridge = new CommunicationBridge();
             bridge.GenerateAirfoil("2412", 150.0);
             return true;
+        }
+
+        /// <summary>
+        /// Registers the add-in in the SolidWorks AddIns registry key.
+        /// </summary>
+        [ComRegisterFunction]
+        public static void RegisterAddin(Type t)
+        {
+            using var key = Registry.CurrentUser.CreateSubKey(@"Software\SolidWorks\AddIns\" + t.GUID.ToString("B"));
+            key.SetValue(null, 1);
+            key.SetValue("Description", "Borzo AI SolidWorks Add-in");
+            key.SetValue("Title", "Borzo Add-in");
+        }
+
+        /// <summary>
+        /// Unregisters the add-in from the SolidWorks AddIns registry key.
+        /// </summary>
+        [ComUnregisterFunction]
+        public static void UnregisterAddin(Type t)
+        {
+            Registry.CurrentUser.DeleteSubKey(@"Software\SolidWorks\AddIns\" + t.GUID.ToString("B"), false);
         }
     }
 }
